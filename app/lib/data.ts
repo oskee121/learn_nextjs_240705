31a33962a9a32075e0913b42f57220b576522602
Keeradit <oskee121@gmail.com>
2024-07-07 00:24:25 +0700

    learn nextjs -6- Streaming components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,6 +30,11 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    console.log('Fetching Latest Invoices data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    console.log('Data fetch completed after 5 seconds.');
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
